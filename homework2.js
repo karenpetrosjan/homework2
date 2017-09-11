8ae29ddffd2dbca7e95aa86952fe0d17845db7a1
karenpetrosjan <petrosyanakaren839@gmail.com>
2017-09-11 13:54:31 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,14 @@
+const num1=2
+const num2=3
+if (num1+num2===5) {
+    console.log("petrosyankaren839@gmail.com")
+} else {
+    console.log("whatever you want to print")
+}
+if (num1*num2===6) {
+    console.log("Photography is life")
+} else {
+    console.log("I did it")
+}
+ 
+
